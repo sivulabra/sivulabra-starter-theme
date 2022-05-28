01586f7d3f33acae4ca73e60670bde7c2e9eec50
Vili Ståhlberg <vkstah@utu.fi>
2022-05-28 13:25:44 +0300

    Added sample REST endpoint

--- functions.php
+++ functions.php
@@ -2,3 +2,6 @@
 
 // Load assets
 require_once 'inc/assets.php';
+
+// Rest endpoints
+require_once 'inc/rest_endpoints';
---
+++ inc/rest_endpoints.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+// Sample endpoint: localhost:3000/wp-json/sivulabra/v1/example
+add_action( 'rest_api_init', function() {
+  register_rest_route(
+    'sivulabra/v1',
+    'example',
+    [
+      'methods' => WP_REST_SERVER::READABLE,
+      'callback' => 'callbackFunction'
+    ]
+    );
+} );
+function callbackFunction() {
+  return 'It works!';
+}
+*/
